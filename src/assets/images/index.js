0334b84bf4c7f6a0b53f44f0ed6826a2c29fef67
Ahsanul HP <hqahsanul@gmail.com>
2024-03-10 11:13:42 +0530

    update

---
+++ src/assets/images/index.js
@@ -0,0 +1,55 @@
+import logo from "./logo.png";
+import logo_alt from "./logo.webp";
+import airlineone from "./airline-one.png";
+import airlinetwo from "./airline-two.png";
+import banner1 from "./banner_1-2-min.jpg";
+import bgone from "./bgone.png";
+import email from "./email.png";
+import favicon from "./favicon.png";
+import Holidays_img_one from "./Holidays-img-one.jpg";
+import Holidays_img_two from "./Holidays-img-two.jpg";
+import imagination1 from "./imagination1.png";
+import imagination2 from "./imagination2.png";
+import imagination3 from "./imagination3.png";
+import payment from "./payment.png";
+import top_dest_hotel_251 from "./top-dest-hotel-251.jpg";
+import top_dest_hotel_798 from "./top-dest-hotel-798.jpg";
+import tour_five from "./tour-five.png";
+import tour_four from "./tour-four.png";
+import tour_one from "./tour-one.png";
+import tour_six from "./tour-six.png";
+import tour_three from "./tour-three.png";
+import tour_two from "./tour-two.png";
+import six_e from "./6E.gif";
+import i_5 from "./I5.gif";
+import stop_5 from "./stop_0.png";
+
+const images = {
+  logo,
+  logo_alt,
+  airlineone,
+  airlinetwo,
+  banner1,
+  bgone,
+  email,
+  favicon,
+  Holidays_img_one,
+  Holidays_img_two,
+  imagination1,
+  imagination2,
+  imagination3,
+  payment,
+  top_dest_hotel_251,
+  top_dest_hotel_798,
+  tour_one,
+  tour_two,
+  tour_three,
+  tour_four,
+  tour_five,
+  tour_six,
+  six_e,
+  i_5,
+  stop_5,
+};
+
+export default images;
